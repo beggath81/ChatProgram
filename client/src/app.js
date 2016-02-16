0f66400c4f204c048089158051aa8ff8e95a4877
beggath81 <beggath81@gmail.com>
2016-02-16 22:49:24 +0000

    adding some routing stuff

--- client/src/app.js
+++ client/src/app.js
@@ -1,20 +1,25 @@
 "use strict";
 
 //hér eru sett inn libary
-angular.mondule("chatApp", ["ui.bootstrap",""]) 
+angular.mondule("chatApp", ["ui.bootstrap","ngRoute"]) 
 
 //hér erum við að tengja síðurnar saman. T.d. loginController fylgir login.html 
 .config(function ($routeProvider) {
-	$routeProvider.when("/", {
+	$routeProvider
+	//þegar ég er á upphafsíðu s.s. í urlinu ChatProgram/
+	.when("/", {     //þá vil ég að login síðan sé sýnileg
 		templateUrl: "src/login/login.html",
 		controller: "LoginContoller"
-	}).when("/roomlist", {
+	})
+	.when("/roomlist", {
 		templateUrl: "src/roomlist/roomlist.html",
 		controller: "RoomlistController"
 		//fer í ákv herbergi með id = ? tekið úr roomControler
-	}).when("/roomlist/id", {
-		templateUrl: ""
+	}).when("/room/id", {
+		templateUrl: "src/room/room.html",
+		controller: "roomControler"
 		//Og svo framleiðis
 	});
+
 
 });

6b13c4d43ab22f36cdc0e5376f77cde28a8779bb
beggath81 <beggath81@gmail.com>
2016-02-16 11:10:01 +0000

    ymislegt

--- client/src/resource/ChatResource.js
+++ client/src/resource/ChatResource.js
@@ -1,1 +1,17 @@
 //here tolum við við bakendann
+"use strict";
+
+angular.module("chatApp").factory("ChatResource", 
+	function ChatResource(){
+		return {
+			//þau föll í bakendanum sem við viljum getað kallað í 
+			login: function login(user, callback){
+				//TODO
+			},
+
+			getRoomList: function getRoomList(callback) {
+				//TODO
+			},
+		}
+
+	});

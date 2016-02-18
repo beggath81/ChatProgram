932b5d14afa4f68b64ab1808ed710c2aa96b29a8
beggath81 <beggath81@gmail.com>
2016-02-18 02:04:20 +0000

    making it grunt ready

--- client/src/app.js
+++ client/src/app.js
@@ -1,25 +1,32 @@
 "use strict";
 
 //hér eru sett inn libary
-angular.mondule("chatApp", ["ui.bootstrap","ngRoute"]) 
+var chatApp = angular.module("chatApp", ["ngRoute"]) 
 
 //hér erum við að tengja síðurnar saman. T.d. loginController fylgir login.html 
-.config(function ($routeProvider) {
+chatApp.config(function ($routeProvider) {
 	$routeProvider
 	//þegar ég er á upphafsíðu s.s. í urlinu ChatProgram/
 	.when("/", {     //þá vil ég að login síðan sé sýnileg
-		templateUrl: "src/login/login.html",
-		controller: "LoginContoller"
+		templateUrl: '/login/login.html',
+		controller: 'LoginContoller'
 	})
 	.when("/roomlist", {
-		templateUrl: "src/roomlist/roomlist.html",
+		templateUrl: "/roomlist.html",
 		controller: "RoomlistController"
 		//fer í ákv herbergi með id = ? tekið úr roomControler
 	}).when("/room/id", {
-		templateUrl: "src/room/room.html",
+		templateUrl: "/room.html",
 		controller: "roomControler"
+		
 		//Og svo framleiðis
-	});
 
+
+		//ef eitthvað annað er slegið inn í urlið þá er farið aftur á upphafssíðu
+	}).otherwise({
+		redirectTo: "/"
+
+
+	});
 
 });
